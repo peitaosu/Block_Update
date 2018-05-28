4d1264e8db2861e1fc04c5527b962fa4b95bb65f
Tony Su <peitaosu@163.com>
2018-05-28 22:15:34 +0800

    modulization

--- map.py
+++ map.py
@@ -1,19 +1,35 @@
-import os, hashlib
+import os, hashlib, json
 
-def get_file_blocks_dict(dir_path, block_size):
-    file_dict = {}
-    for root, dirs, files in os.walk(dir_path):
-        for file_item in files:
-            file_path = os.path.join(root, file_item)
-            file_dict[file_path] = get_blocks_hash(file_path, block_size)
-    return file_dict
+class BlockMap():
+    
+    def __init__(self):
+        self.dir_path = None
+        self.block_size = 4 * 1024
+        self.map = {}
+    
+    def set_dir_path(self, dir_path):
+        self.dir_path = dir_path
+    
+    def set_block_size(self, block_size):
+        self.block_size = block_size
+    
+    def get_blocks_hash(self, file_path, block_size):
+        f = open(file_path)
+        hash_list = []
+        while True:
+            data = f.read(block_size)
+            if not data:
+                break
+            hash_list.append(hashlib.md5(data).hexdigest())
+        return hash_list
 
-def get_blocks_hash(file_path, block_size):
-    f = open(file_path)
-    hash_list = []
-    while True:
-        data = f.read(block_size)
-        if not data:
-            break
-        hash_list.append(hashlib.md5(data).hexdigest())
-    return hash_list
+    def get_blocks_map(self):
+        for root, dirs, files in os.walk(self.dir_path):
+            for file_item in files:
+                file_path = os.path.join(root, file_item)
+                self.map[file_path] = self.get_blocks_hash(file_path, self.block_size)
+        return self.map
+    
+    def save_map(self, save_path):
+        with open(save_path, "w") as save_file:
+            json.dump(self.map, save_file)
