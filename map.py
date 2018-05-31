50d2ea89a35580e1e118ed5bb265747b9c32d008
Tony Su <peitaosu@163.com>
2018-05-31 22:10:53 +0800

    diff as attribute

--- map.py
+++ map.py
@@ -8,6 +8,7 @@ class BlockMap():
         self.diff_file = "diff.json"
         self.block_size = 4 * 1024
         self.map = {}
+        self.diff = None
     
     def set_dir_path(self, dir_path):
         self.dir_path = dir_path
@@ -47,71 +48,69 @@ class BlockMap():
         if self.block_size != target.block_size:
             print "Cannot diff because block maps created with different block size."
             return None
-        diff = {
+        self.diff = {
             "added": [],
             "removed": [],
             "updated": {}
         }
-        diff["added"] = [i for i in self.map if i not in target.map]
-        diff["removed"] = [i for i in target.map if i not in self.map]
+        self.diff["added"] = [i for i in self.map if i not in target.map]
+        self.diff["removed"] = [i for i in target.map if i not in self.map]
         same = [i for i in self.map if i in target.map]
         for i in same:
             if self.map[i] == target.map[i]:
                 continue
             length = max(len(self.map[i]), len(target.map[i]))
-            diff["updated"][i] = []
+            self.diff["updated"][i] = []
             for x in range(length):
                 try:
                     if self.map[i][x] == target.map[i][x]:
-                        diff["updated"][i].append({})
+                        self.diff["updated"][i].append({})
                     else:
-                        diff["updated"][i].append({
+                        self.diff["updated"][i].append({
                             "target": target.map[i][x],
                             "upgrade": self.map[i][x]
                         })
                 except IndexError:
                     if x < len(self.map[i]):
-                        diff["updated"][i].append({
+                        self.diff["updated"][i].append({
                             "target": "",
                             "upgrade": self.map[i][x]
                         })
                     else:
-                        diff["updated"][i].append({
+                        self.diff["updated"][i].append({
                             "target": target.map[i][x],
                             "upgrade": ""
                         })
-        return diff
 
-    def create_diff(self, target):
-        diff = self.diff_map(target)
-        if diff is None:
+    def create_diff(self):
+        if self.diff is None:
             return False
-        for add_file in diff["added"]:
+        for add_file in self.diff["added"]:
             source = os.path.join(self.dir_path, add_file)
             dest = os.path.join(self.diff_path, add_file)
             if not os.path.isdir(os.path.dirname(dest)):
                 os.makedirs(os.path.dirname(dest))
             shutil.copyfile(source, dest)
-        for update_file in diff["updated"]:
+        for update_file in self.diff["updated"]:
             upgrade_file = os.path.join(self.dir_path, update_file)
             upgrade_diff = os.path.join(self.diff_path, update_file)
             if not os.path.isdir(os.path.dirname(upgrade_diff)):
                 os.makedirs(os.path.dirname(upgrade_diff))
-            length = len(diff["updated"][update_file])
+            length = len(self.diff["updated"][update_file])
             with open(upgrade_file, "rb") as in_file:
                 for i in range(length):
                     data = in_file.read(self.block_size)
-                    if "upgrade" in diff["updated"][update_file][i] and diff["updated"][update_file][i]["upgrade"] != "":
-                        diff_file = upgrade_diff + "-" + diff["updated"][update_file][i]["upgrade"]
+                    if "upgrade" in self.diff["updated"][update_file][i] and self.diff["updated"][update_file][i]["upgrade"] != "":
+                        diff_file = upgrade_diff + "-" + self.diff["updated"][update_file][i]["upgrade"]
                         with open(diff_file, 'wb') as out_file:
                             out_file.write(data)
                     else:
                         continue
         diff_file_path = os.path.join(os.path.dirname(self.diff_path), self.diff_file)
         with open(diff_file_path, "w") as save_file:
-            json.dump(diff, save_file, indent=4)
+            json.dump(self.diff, save_file, indent=4)
         return True
-
+    
 def get_options():
     parser = optparse.OptionParser()
     parser.add_option("-u", "--upgrade", dest="upgrade",
@@ -143,5 +142,6 @@ if __name__ == "__main__":
     upgrade.get_blocks_map()
     target.get_blocks_map()
     
+    upgrade.diff_map(target)
     upgrade.set_diff_path(opt.diff)
-    upgrade.create_diff(target)
+    upgrade.create_diff()
